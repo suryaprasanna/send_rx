6d8935ae181556e6931b5826f34dd7cea07a32fe
suryayalla <prasannakumar006@gmail.com>
2017-07-07 16:45:38 -0400

    add few methods to query the database

--- lock_forms.php
+++ lock_forms.php
@@ -2,24 +2,36 @@
 
 class lock_form {
 	var $username;
-	var $pid;
+	var $project_id;
 	var $record_id;
-	var $event_id;
-	var $form_name;
-	var $instance;
-	var $lock_all;
 
-
-	function lock_form ($username, $pid, $record_id, $event_id, $form_name, $instance=1, $lock_all= false) {
+	function lock_form ($username, $project_id, $record_id) {
 		$this->username = $username;
-		$this->pid = $pid;
+		$this->project_id = $project_id;
 		$this->record_id = $record_id;
-		$this->event_id = $event_id;
-		$this->form_name = $form_name;
-		$this->instance = $instance;
-		$this->lock_all = $lock_all;
 	}
-	
+
+	function lockCurrentForm($event_id, $form_name, $instance=1) {
+
+	}
+
+	function lockAllFormsInEvent($event_id) {
+
+	}
+
+	function getForm
+
+	function insertData($event_id, $form_name, $instance=1) {
+		$sql = "INSERT INTO redcap_locking_data (project_id, record, event_id, form_name, instance, username, timestamp ) VALUES (?, ?, ?, ?, ?, ?, now())";
+		if ($stmt=$this->conn->prepare($sql)) {
+			$stmt->bind_param("isisis", $this->project_id, $this->record_id, $event_id, $form_name, $instance, $this->username);
+			if ($stmt->execute()) {
+			// echo "New record created successfully." . '<br>';
+				return;
+			}
+		}
+	}
+
 }
 
 ?>
